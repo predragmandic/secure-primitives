efb4142957aaea628d7f9315e92ecad4495f371c
Predrag Mandic <pmandic@gmx.de>
2012-11-20 16:18:17 +0100

    Finished SecurePrim<T> template class. Tested it with int, long int, double and const char* values.

--- src/secureprim.hpp
+++ src/secureprim.hpp
@@ -9,26 +9,39 @@
  
 
 #include <stdlib.h>
-#include <limits.h>
 #include <iostream>
+//#include <limits.h>
 
-
-
-
-template <class T = int> class SecurePrim {
+template <class T = unsigned char> class SecurePrim {
   
 private:
-  T m_nSecurePrimPartA;
-  T m_nSecurePrimPartB;
+  
+  unsigned char m_bSecurePrimPartA[sizeof(T)];
+  unsigned char m_bSecurePrimPartB[sizeof(T)];
+  
+  void serialize(T &value, unsigned char (&charArray)[sizeof(T)]);
+  
+  void encodeArray(
+              unsigned char (&charArray)[sizeof(T)],
+              unsigned char (&securePartA)[sizeof(T)],
+              unsigned char (&securePartB)[sizeof(T)]
+            );
+  
+  void decodeArray(
+            const unsigned char (&securePartA)[sizeof(T)],
+            const unsigned char (&securePartB)[sizeof(T)],
+            unsigned char (&charArray)[sizeof(T)]
+          ) const;
   
   T decode() const;
+  
   SecurePrim& set(const SecurePrim& other);
   SecurePrim& set(T value);
-  unsigned char* serialize(T &value);
   
 public:
   SecurePrim(T value = 0);
   operator T() { return decode(); }
+  
   
   SecurePrim<T>& operator=(const SecurePrim& other);
   SecurePrim<T>& operator=(const T& value);
@@ -61,138 +74,164 @@ public:
 
 
 
-
-
-
-
-
-
 // private functions
+
+//serialize
 template <class T>
-T SecurePrim<T>::decode() const {
-  return this->m_nSecurePrimPartA ^ this->m_nSecurePrimPartB;
+void SecurePrim<T>::serialize(T &value, unsigned char (&charArray)[sizeof(T)]) {
+  std::copy(
+    static_cast<const char*>(static_cast<const void*>(&value)),
+    static_cast<const char*>(static_cast<const void*>(&value)) + sizeof(value),
+    charArray);
 }
 
+//encodeArray
+template <class T>
+void SecurePrim<T>::encodeArray(
+                                unsigned char (&charArray)[sizeof(T)],
+                                unsigned char (&securePartA)[sizeof(T)],
+                                unsigned char (&securePartB)[sizeof(T)]
+                              )
+{
+  for(unsigned int i=0; i<sizeof(T); i++) {
+    securePartA[i] = rand() % sizeof(char);
+    securePartB[i] = securePartA[i] ^ charArray[i];
+  }
+}
+
+//decodeArray
+template <class T>
+void SecurePrim<T>::decodeArray(
+            const unsigned char (&securePartA)[sizeof(T)],
+            const unsigned char (&securePartB)[sizeof(T)],
+            unsigned char (&charArray)[sizeof(T)]
+          ) const
+{
+  for(unsigned int i=0; i<sizeof(T); i++) {
+    charArray[i] = securePartA[i] ^ securePartB[i];
+  }
+}
+
+//decode
+template <class T>
+T SecurePrim<T>::decode() const {
+  unsigned char decodedArray[sizeof(T)];
+  decodeArray(m_bSecurePrimPartA, m_bSecurePrimPartB, decodedArray);
+  return *reinterpret_cast<T*>(decodedArray);
+}
+
+
+//set
 template <class T>
 SecurePrim<T>& SecurePrim<T>::set(const SecurePrim<T>& other) {
-  this->m_nSecurePrimPartA = rand();// % UINT_MAX;
-  this->m_nSecurePrimPartB = other.decode() ^ this->m_nSecurePrimPartA;
-  std::cout << m_nSecurePrimPartA;
-  return *this;
+  return set(other.decode());
 }
 
 template <class T>
 SecurePrim<T>& SecurePrim<T>::set(T value) {
-  this->m_nSecurePrimPartA = rand();// % UINT_MAX;
-  this->m_nSecurePrimPartB = value ^ this->m_nSecurePrimPartA;
-  std::cout << m_nSecurePrimPartA;
+  unsigned char serializedPrim[sizeof(T)];
+  serialize(value, serializedPrim);
+  encodeArray(serializedPrim, m_bSecurePrimPartA, m_bSecurePrimPartB);
   return *this;
-}
-
-template <class T>
-unsigned char* SecurePrim<T>::serialize(T &value) {
-  return reinterpret_cast<unsigned char*>(&value);
 }
 
 
 // constructor
 template <class T>
 SecurePrim<T>::SecurePrim(T value) {
-  this->set(value);
+  set(value);
 }
 
 // =
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator=(const SecurePrim<T>& other) {
-  return this->set(other);
+  return set(other);
 }
 
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator=(const T& value) {
-  return this->set(value);
+  return set(value);
 }
 
 // +=
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator+=(const SecurePrim<T>& other) {
-  return this->set(this->decode() + other.decode());
+  return set(decode() + other.decode());
 }
 
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator+=(const T& value) {
-  return this->set(this->decode() + value);
+  return set(decode() + value);
 }
 
 // ++x
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator++() {
-  this->set(this->decode()+1);
-  return *this;
+  return set(decode() + 1);
 }
 
 // x++
 template <class T>
 SecurePrim<T> SecurePrim<T>::operator++(int) {
-  SecurePrim<T> pre_increment = this->decode();
-  this->set(this->decode()+1);
+  SecurePrim<T> pre_increment = decode();
+  set(decode() + 1);
   return pre_increment;
 }
 
 // -=
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator-=(const SecurePrim<T>& other) {
-  return this->set(this->decode() - other.decode());
+  return set(decode() - other.decode());
 }
 
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator-=(const T& value) {
-  return this->set(this->decode() - value);
+  return set(decode() - value);
 }
 
 // --x
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator--() {
-  this->set(this->decode()-1);
-  return *this;
+  return set(decode() - 1);
 }
 // x--
 template <class T>
 SecurePrim<T> SecurePrim<T>::operator--(int) {
-  SecurePrim<T> pre_increment = this->decode();
-  this->set(this->decode()-1);
+  SecurePrim<T> pre_increment = decode();
+  set(decode() - 1);
   return pre_increment;
 }
 
 // *=
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator*=(const SecurePrim<T>& other) {
-  return this->set(this->decode() * other.decode());
+  return set(decode() * other.decode());
 }
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator*=(const T& value) {
-  return this->set(this->decode() * value);
+  return set(decode() * value);
 }
 
 // /=
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator/=(const SecurePrim<T>& other) {
-  return this->set(this->decode() / other.decode());
+  return set(decode() / other.decode());
 }
 
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator/=(const T& value) {
-  return this->set(this->decode() / value);
+  return set(decode() / value);
 }
 
 // %=
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator%=(const SecurePrim<T>& other) {
-  return this->set(this->decode() % other.decode());
+  return set(decode() % other.decode());
 }
 
 template <class T>
 SecurePrim<T>& SecurePrim<T>::operator%=(const T& value) {
-  return this->set(this->decode() % value);
+  return set(decode() % value);
 }
 
 
--- src/test.cpp
+++ src/test.cpp
@@ -11,16 +11,23 @@
 #include <time.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string.h>
 
+#define SHORT_INT SecurePrim<short int>
+#define INT SecurePrim<int>
+#define LONG_INT SecurePrim<long int>
+#define FLOAT SecurePrim<float>
+#define DOUBLE SecurePrim<double>
+#define LONG_DOUBLE SecurePrim<long double>
+#define STRING SecurePrim<const char *>
 
 int main() {
   srand ( time(0) );
 
-  std::cout << "\nproba1 = 10"; SecurePrim<unsigned long int> proba1=10;
-  std::cout << "\nproba2 = 5";  SecurePrim<unsigned long int> proba2=5;
-  std::cout << "\nproba3 = 3";  SecurePrim<unsigned long int> proba3=3;
-  std::cout << "\nproba";
-  SecurePrim<unsigned long int> proba;
+  std::cout << "\nproba1 = 10"; LONG_INT proba1=10;
+  std::cout << "\nproba2 = 5";  LONG_INT proba2=5;
+  std::cout << "\nproba3 = 3";  LONG_INT proba3=3;
+  std::cout << "\nproba"; LONG_INT proba;
   std::cout << "\n" << proba << "\n\n";
   
   std::cout << "\nproba = 40";
@@ -110,6 +117,35 @@ int main() {
   std::cout << "\n" << proba;
   std::cout << "\n" << proba3;
   std::cout << "\n\n";
+  
+  
+  std::cout << "\nzsproba1 = jedan";  STRING zsproba1= "jedan";
+  std::cout << "\nzsproba2 = dva";    STRING zsproba2= "dva";
+  std::cout << "\nzsproba3 = tri";    STRING zsproba3= "tri";
+  std::cout << "\n" << zsproba1;
+  std::cout << "\n" << zsproba2;
+  std::cout << "\n" << zsproba3;
+  
+  zsproba1=zsproba3;
+  zsproba2=zsproba3;
+  std::cout << "\n" << zsproba1;
+  std::cout << "\n" << zsproba2;
+  std::cout << "\n" << zsproba3;
+  
+  zsproba3="TRI";
+  std::cout << "\n" << zsproba1;
+  std::cout << "\n" << zsproba2;
+  std::cout << "\n" << zsproba3;
+  
+  zsproba1="JEDAN";
+  zsproba2="DVA";
+  std::cout << "\n" << zsproba1;
+  std::cout << "\n" << zsproba2;
+  std::cout << "\n" << zsproba3;
+  
+  
+  
+  
   
   //std::cout << "\n";
   
